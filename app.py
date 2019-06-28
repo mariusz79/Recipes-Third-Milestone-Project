d0864a8caba78792e151e93443acd0f1dda77878
mariusz79 <mpawlowic@gmail.com>
2019-06-28 19:00:55 +0100

    connect to MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, flash, session
+from flask_pymongo import PyMongo, pymongo
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI1')  # 'MONGO_URI1' is saved in environment variables, to hide password
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
